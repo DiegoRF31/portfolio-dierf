472eb9c6ced4f48b99ea8913eeae1cbfef6ff1fd
Diego Rodriguez <diego.andres6716@gmail.com>
2025-07-16 01:00:28 -0600

    Screen changes and add the environment cretentials for future email feature

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  SERVICE_MAIL_ID:'service_oyn38p9',
+  TEMPLATE_MAIL_ID:'template_08p7ahg',
+  PUBLIC_KEY_MAIL_ID:'5V6t1T6iUie-CkHoR',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  SERVICE_MAIL_ID:'service_oyn38p9',
+  TEMPLATE_MAIL_ID:'template_08p7ahg',
+  PUBLIC_KEY_MAIL_ID:'5V6t1T6iUie-CkHoR',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
