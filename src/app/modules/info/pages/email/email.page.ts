5d4744e16a097239ee74951fd3f68fecdb55cd81
Diego Rodriguez <diego.andres6716@gmail.com>
2025-07-24 00:29:31 -0600

    Configure the Email component, format and functionality in progress

--- src/app/modules/info/pages/email/email.page.ts
+++ src/app/modules/info/pages/email/email.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/app/environments/environment';
+import { validationMessage } from 'src/types/general.types';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-email',
@@ -7,5 +13,88 @@ import { Component } from '@angular/core';
   styleUrl: './email.page.css'
 })
 export class EmailPage {
+  public formContact: FormGroup;
+  public sending = false
+  public validations = [
+    {
+      input: 'name',
+      validations: [{
+        key: 'required',
+        message: 'VALIDATIONS.NOMBRE_REQUERIDO'
+      }]
+    },
+    {
+      input: 'email',
+      validations: [
+        {
+          key: 'required',
+          message: 'VALIDATIONS.EMAIL_REQUERIDO'
+        },
+        {
+          key: 'pattern',
+          message: 'VALIDATIONS.EMAIL_INVALIDO'
+        },
+      ]
+    },
+    {
+      input: 'message',
+      validations: [
+        {
+          key: 'required',
+          message: 'VALIDATIONS.MENSAJE_REQUERIDO'
+        },
+        {
+          key: 'minlength',
+          message: 'VALIDATIONS.MENSAJE_LONGITUD_MINIMA'
+        },
+        {
+          key: 'maxlength',
+          message: 'VALIDATIONS.MENSAJE_LONGITUD_MAXIMA'
+        }
+      ]
+    }
+  ]
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+    this.formContact = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      message: ['', [Validators.required, Validators.minLength(30), Validators.maxLength(500)]],
+    });
+  }
+
+  inputValidate(key: string): validationMessage[] {
+    const listValidationes = this.validations.find(i => i.input === key)
+    if (listValidationes) {
+      return listValidationes.validations
+    }
+    return []
+  }
+
+  get form() {
+    return this.formContact.controls;
+  }
+
+  sendMessage() {
+    this.sending = true
+    if (this.formContact.invalid) {
+      this.form['name'].markAsDirty()
+      this.form['email'].markAsDirty()
+      this.form['message'].markAsDirty()
+      this.sending = false
+      return
+    }
+    emailjs.send(environment.SERVICE_MAIL_ID, environment.TEMPLATE_MAIL_ID, this.formContact.value, environment.PUBLIC_KEY_MAIL_ID)
+      .then(() => {
+        this.sending = false
+        this.router.navigate(['success'])
+      }, (err) => {
+        console.log('error, recuerda crear tu cuenta en EmailJs. En el Readme.md de este proyecto esta como realizar la instalación  ')
+        console.log('error ==>', err)
+        this.sending = false
+      });
+  }
 }
